c52e8faccbd94bc484613feb4479ac1e909dfcb9
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-17 18:17:56 +0000

    Reworked to centralise navbar to a single file, updated a few tables to table format

---
+++ shared/navbar.js
@@ -0,0 +1,33 @@
+document.write(`\
+\
+   <ul class="navbar">\
+\
+      <li class="navListL">\
+         <div>\
+            Alternion\
+         </div>\
+      </li>\
+\
+      <li class="navListL">\
+         <a href="http://www.archiesbots.com/AlternionWiki/Home.html">Home</a>\
+      </li>\
+\
+      <li class="navListL">\
+         <a href="http://www.archiesbots.com/AlternionWiki/Main-Sails.html">Main Sails</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="http://www.archiesbots.com/index.html">Archies Bots</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="http://www.archiesbots.com/AlternionWiki/FAQ.html">FAQ</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="http://www.archiesbots.com/AlternionWiki/About.html">About</a>\
+      </li>\
+\
+   </ul>\
+\
+`);
