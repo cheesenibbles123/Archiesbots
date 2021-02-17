7c5c6a70ef8565a7b83be399e6753689d9954be3
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-17 20:46:42 +0000

    Fixed bottom bar not showing on mainsails

--- shared/bottombar.js
+++ shared/bottombar.js
@@ -1,11 +1,11 @@
 document.write(`\
 	<ul class="bottomBar">\
 \
-     <li style="text-align: center;">\
-        <div>\
-           <a href="https://github.com/cheesenibbles123">Alternion ©️ Cheesenibbles123</a>\
-        </div>\
-     </li>\
+	     <li style="text-align: center;">\
+	        <div>\
+	           <a href="https://github.com/cheesenibbles123">Alternion ©️ Cheesenibbles123</a>\
+	        </div>\
+	     </li>\
 \
-  </ul>\
+	</ul>\
 `);
