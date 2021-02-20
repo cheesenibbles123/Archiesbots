430ec4eda6e95392cc4cf3012308a8c29db704df
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-20 20:30:59 +0000

    Added bots setup

---
+++ Discord-Bots/shared/bottombar.js
@@ -0,0 +1,11 @@
+document.write(`\
+	<ul class="bottomBar">\
+\
+	     <li style="text-align: center;">\
+	        <div>\
+	           <a href="https://github.com/cheesenibbles123">Discord Bots ©️ Cheesenibbles123</a>\
+	        </div>\
+	     </li>\
+\
+	</ul>\
+`);
---
+++ Discord-Bots/shared/navbar.js
@@ -0,0 +1,32 @@
+document.write(`\
+   <ul class="navbar">\
+\
+      <li class="navListL">\
+         <div>\
+            Discord Bots\
+         </div>\
+      </li>\
+\
+      <li class="navListL">\
+         <a href="#">Home</a>\
+      </li>\
+\
+      <li class="navListL">\
+         <a href="#">Oliverbot</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="#">Archies Bots</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="#">FAQ</a>\
+      </li>\
+\
+      <li class="navListR">\
+         <a href="#">About</a>\
+      </li>\
+\
+   </ul>\
+\
+`);
