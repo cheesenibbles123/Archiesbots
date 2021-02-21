a32e9e226f0a8bdb6a97f8a92c8bd05001e7b054
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-21 11:15:42 +0000

    Added intial drop down btn to switch between alternion and bots

--- Discord-Bots/shared/navbar.js
+++ Discord-Bots/shared/navbar.js
@@ -1,10 +1,19 @@
 document.write(`\
+\
+   <script>\
+      function myFunction(itemID) {\
+         document.getElementById(itemID).classList.toggle("show");\
+      }\
+   </script>\
+\
    <ul class="navbar">\
 \
       <li class="navListL">\
-         <div>\
-            Discord Bots\
-         </div>\
+         <button onclick="myFunction('mainHome')">Discord Bots</button>\
+         <div id="mainHome" style="visibility: hidden;">
+            <a href="http://www.archiesbots.com/AlternionWiki/Home.html">Alternion</a>
+            <a href="#">Discord Bots</a>
+         </div>
       </li>\
 \
       <li class="navListL">\
