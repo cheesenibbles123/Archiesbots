09bb87b662e298ec56f57a2d4c81cc7a774733dc
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-03-07 15:56:24 +0000

    added diamond skin set file

---
+++ AlternionWiki/Skins/WeaponSkins/Diamond.js
@@ -0,0 +1,21 @@
+document.write(`
+	<details>
+  		<summary class="infoBox">Mosaic</summary>
+          <table class="tableLayout">
+             <tr>
+                <th>Name</th>
+                <th>ID</th>
+                <th>Creator</th>
+                <th>Image</th>
+             </tr>
+
+             <tr>
+                <td>Mosaic Axe</td>
+                <td><code>axe_mosaic</code></td>
+                <td>Archie</td>
+                <td><img alt="Mosaic Axe" src="" height="320" width="180"></td>
+             </tr>
+
+          </table>
+    </details>
+`);
