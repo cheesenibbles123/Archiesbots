f6307fa7a78a93b07ee4f8471c908a9875c7cae2
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-03-07 15:46:39 +0000

    Setup initial weapon-skins template

---
+++ AlternionWiki/Skins/WeaponSkins/Royal.js
@@ -0,0 +1,21 @@
+document.write(`
+	<details>
+  		<summary class="infoBox">Royal</summary>
+          <table class="tableLayout">
+             <tr>
+                <th>Name</th>
+                <th>ID</th>
+                <th>Creator</th>
+                <th>Image</th>
+             </tr>
+
+             <tr>
+                <td>Royal Axe</td>
+                <td><code>axe_royal</code></td>
+                <td>Archie</td>
+                <td><img alt="Royal Axe" src="" height="200" width="200"></td>
+             </tr>
+
+          </table>
+    </details>
+`);
