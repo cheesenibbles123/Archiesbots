b9b05abadd0a6b39eddbc3dab7de7b9c83f3ca0b
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-03-07 16:34:17 +0000

    Converted contents menu to standalone file

---
+++ AlternionWiki/Skins/WeaponSkins/Contents.js
@@ -0,0 +1,30 @@
+document.write(`
+	<div class="contentsBacking">
+        <h2 style="border-bottom: solid 1px #000000; text-align:center;">
+          Contents
+        </h2>
+        <ul class="listNoBulletPoint">
+
+          <!-- Public -->
+            <li>
+                Public
+                <ul>
+                    <li>
+                        Single Colours
+                    </li>
+                    <li>
+                        Sets
+                        <ul>
+                            <li>
+                                Mosaic
+                            </li>
+                            <li>
+                                Royal
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+`);
